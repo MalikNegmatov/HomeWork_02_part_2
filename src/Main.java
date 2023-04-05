d8c08f3dff2ccda22f97da2eec51ec02b7f27d29
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-06 00:03:28 +0300

    Инициализация мастер ветки проекта Домашнее задание Урок №2 часть 2.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+// Begin main
+    {
+            var taskCount = 0;
+            var taskScreen = "Задача №";
+
+// ======= Begin Task 1 =========
+
+            taskCount++;
+            System.out.println(taskScreen + taskCount);
+
+// ======== End Task 1 ==========
+
+    }
+// End main
+
+}
